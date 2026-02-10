8d490289453687a8989ae7e82b7732ac6c344df4
nabil-hajali <nabil2491@gmail.com>
2026-02-10 15:46:38 +0100

    index com

--- main.js
+++ main.js
@@ -1,30 +1,49 @@
 //chiedere all'utente di inserire una parola
 
-const parola = prompt('inserisci una parola');
-const text = parola;
+//const parola = prompt('inserisci una parola');
+//const text = parola;
 
 //creare una funzione per capire se la parola inserita è palindroma
 
-function isPalindroma(text) {
-    
+//function isPalindroma(text) {
+
     // rendo tutto minuscolo
-    text = text.toLowerCase();
+   // text = text.toLowerCase();
 
     // inverto la stringa
-    const textReverse = text.split('').reverse().join('');
+    //const textReverse = text.split('').reverse().join('');
 
     // confronto se sono uguali
-    return text === textReverse;
-}
+  //  return text === textReverse;
+//}
 // condizione per sapere se è palindroma o meno
 
-if (isPalindroma(parola)) {
-    console.log("La parola è palindroma");
-} else {
-    console.log("La parola non è palindroma");
+//if (isPalindroma(parola)) {
+  //  console.log("La parola è palindroma");
+//} else {
+  //  console.log("La parola non è palindroma");
+//}
+
+
+
+// pari e dispari 
+
+
+const userDice = prompt('scegli un numero da 1 a 5');
+const computerDice = Math.floor(Math.random() * 5) + 1
+
+function 
+
+function logRolledDice(){
+    const dice = Math.floor(Math.random() * 5) + 1
+    console.log(`You rolled the dice, result: ${dice}`);
 }
+logRolledDice()
 
-
+function printRolledDice(){
+    const dice = Math.floor(Math.random() * 5) + 1
+    const message = ``
+}
 
 
  
